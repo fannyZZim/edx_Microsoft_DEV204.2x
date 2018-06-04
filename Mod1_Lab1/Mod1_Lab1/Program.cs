e59b3b292e53dc31b63c104251c93ac94169292e
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-03 20:47:54 -0400

    vis studio

---
+++ Mod1_Lab1/Mod1_Lab1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Mod1_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating an Object of Car() Class by using Type Inference called Carl
+            var Carl = new Car();
+
+            // Using dot notation to call members on Carl
+            Carl.Color = "White";
+            Carl.Year = 2010;
+            Carl.Mileage = 11000;
+
+            // Output to the console window
+            Console.WriteLine($"This car is painted {Carl.Color}, was built in {Carl.Year}, and has {Carl.Mileage} miles on it.");
+        }
+    }
+    // Declaring the Car() Class
+    // This class has 3 properties: Color, Year, and Mileage
+    public class Car
+    {
+        // Defining properties
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+    }
+}
