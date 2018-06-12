14f1b12b2f54924d072ef4f6ed653750d5b63aff
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-11 20:19:29 -0400

    Add tech employee

--- Mod2_Lab1/TechnicalEmployee.cs
+++ Mod2_Lab1/TechnicalEmployee.cs
@@ -1,16 +1,24 @@
-namespace edx_Microsoft_DEV204.2x.Mod2_Lab1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_Lab1
 {
     // TechnicalEmployee Class inheriting from Employee Class
-    public class TechnicalEmployee : Employee
+    class TechnicalEmployee : Employee
     {
+        // Creates integer variable called "successfulCheckIns" and assigns value to 5
+        public int successfulCheckIns = 5;
+
         // Calls upon base-class (Employee Class) constructor from within derived class (TechnicalEmployee Class)
         // Sets baseSalary to 75000 for all TechnicalEmployee objects
         public TechnicalEmployee(String name) : base(name, 75000)
         {
-            
+
         }
-        // Creates integer variable called "successfulCheckIns" and assigns value to 5
-        public int successfulCheckIns = 5;
+
         // This method returns the toString() method - which is the employee's ID number and name - and prints the number of successful check ins the employee has had 
         public override String employeeStatus()
         {
