a7b99e9aa602c3bbfa2d9323dd55421b1ba9b0c1
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-11 20:20:14 -0400

    Add business employee

--- Mod2_Lab1/BusinessEmployee.cs
+++ Mod2_Lab1/BusinessEmployee.cs
@@ -1,19 +1,27 @@
-namespace edx_Microsoft_DEV204.2x.Mod2_Lab1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_Lab1
 {
     // BusinessEmployee Class inheriting from Employee Class
     class BusinessEmployee : Employee
     {
-    // Calls upon base-class (Employee Class) constructor from within derived class (BusinessEmployee Class)
-    // Sets baseSalary to 50000 for all BusinessEmployee objects
-    public BusinessEmployee(String name) : base(name, 50000)
-    {
-    } 
-    // Creates double variable called "bonusBudget" and assigns value to 1000
-    public double bonusBudget = 1000;
-    bonus budget
-    public override String employeeStatus()
-    {
-        return toString() + " with a budget of " + this.bonusBudget;
-    }
+        // Creates double variable called "bonusBudget" and assigns value to 1000
+        public double bonusBudget = 1000;
+
+        // Calls upon base-class (Employee Class) constructor from within derived class (BusinessEmployee Class)
+        // Sets baseSalary to 50000 for all BusinessEmployee objects
+        public BusinessEmployee(String name) : base(name, 50000)
+        {
+        }
+
+        // This method returns the toString() method - which is the employee's ID number and name - and prints the bonus budget
+        public override String employeeStatus()
+        {
+            return toString() + " with a budget of " + this.bonusBudget;
+        }
     }
 }
