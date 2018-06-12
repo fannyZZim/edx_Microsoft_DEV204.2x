540f33b2c802f4a85732ab25c03ca40f81210cc1
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-12 18:05:32 -0400

    Add changes

--- Mod1_SA_Lab/Program.cs
+++ Mod1_SA_Lab/Program.cs
@@ -1,21 +1,15 @@
 ﻿using System;
-<<<<<<< HEAD
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Mod1_SA_Lab
-=======
-
-namespace edx_Microsoft_DEV204._2x
->>>>>>> WIP
 {
     class Program
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             // Instantiating an object of Car() Class by using Type Inference called Car1
             var UProgram1 = new UProgram();
             // Using dot notation to call members on Car1
@@ -58,64 +52,3 @@ namespace edx_Microsoft_DEV204._2x
         }
     }
 }
-=======
-            DrinksMachine myMachine = new DrinksMachine();
-            myMachine._location = "Kitchen";
-            myMachine._model = "DM1000";
-
-            Console.WriteLine(myMachine._location);
-        }
-    }
-}
-
-// Defining Class Members
-public class DrinksMachine
-{
-   // The following statements define a property with a private field.
-   private string _location;
-   public string Location
-   {
-      get
-      {
-         return _location;
-      }
-      set
-      {
-         if (value != null) 
-            _location = value;
-      }
-   }
-   // The following statements define properties.
-   public string Make {get; set;}
-   public string Model {get; set;}
-   // The following statements define methods.
-   public void MakeCappuccino()
-   {
-      // Method logic goes here.
-   }
-   public void MakeEspresso()
-   {
-      // Method logic goes here.
-   }
-   // The following statement defines an event. The delegate definition is not shown.
-   public event OutOfBeansHandler OutOfBeans;
-}
-
-public partial class DrinksMachine
-{
-
-   public void MakeCappuccino()
-   {
-      // Method logic goes here.
-   }
-}
-
-public partial class DrinksMachine
-{
-
-   public void MakeEspresso()
-   {
-      // Method logic goes here.
-   }
-}
->>>>>>> WIP
