25ca84f47c9d8c3addf5772ea4b21d8b90c74945
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-07-02 16:22:12 -0400

    add this

--- Mod2_SA_Lab/Person.cs
+++ Mod2_SA_Lab/Person.cs
@@ -1,7 +1,67 @@
 namespace edx_Microsoft_DEV204.2x.Mod2_SA_Lab
 {
-    public class Person
+    abstract class Person
     {
-        
+       // Creates integer variable called "personCount" and assigns value to 1
+        private static int personCount = 1;
+
+        // 2 private member variables: personName, personBaseSalary, and personId
+        private string personName;
+        private int personId;
+
+        // Public properties
+        public string Name
+        {
+            get
+            {
+                return personName;
+            }
+
+            set
+            {
+                personName = value;
+            }
+        }
+        public int ID
+        {
+            get
+            {
+                return personId;
+            }
+
+            set
+            {
+                personId = value;
+            }
+        }
+
+        // Constructor
+        public person(String name)
+        {
+            this.Name = name;
+            this.ID = personCount++;
+        }
+
+        // This method returns the person's name
+        public String getName()
+        {
+            return this.Name;
+        }
+
+        // This method returns the person's ID
+        public int getPersonID()
+        {
+            return this.ID;
+        }
+
+        // This method returns the person's ID and Name
+        public String toString()
+        {
+            return this.ID + " " + this.Name;
+        }
+
+        // Abstract method
+        public abstract String personStatus();
+     
     }
 }
